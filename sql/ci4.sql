53c1cb43c3bcfc06322742f1f45fac550ff5f081
Rikoairlan <rikoairlan57@gmail.com>
2022-03-16 11:14:01 +0700

    menambahkan database

---
+++ sql/ci4.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 16 Mar 2022 pada 05.12
+-- Versi server: 10.4.22-MariaDB
+-- Versi PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ci4`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `komik`
+--
+
+CREATE TABLE `komik` (
+  `id` int(11) NOT NULL,
+  `judul` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `penulis` varchar(255) NOT NULL,
+  `penerbit` varchar(255) NOT NULL,
+  `sampul` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `komik`
+--
+
+INSERT INTO `komik` (`id`, `judul`, `slug`, `penulis`, `penerbit`, `sampul`, `created_at`, `updated_at`) VALUES
+(1, 'Fairy Tail', 'fairy-tail', 'Hiro Mashima', 'Shonen Jump', 'fairytail.jpg', '2022-03-07 09:21:57', '2022-03-07 09:21:57'),
+(2, 'One Piece', 'one-piece', 'Eiichiro Oda', 'Shonen Jump', 'one-piece.jpg', '2022-03-07 09:21:57', '2022-03-07 09:21:57'),
+(4, 'Dragon Ball Z', 'dragon-ball-z', 'Akira Toriyama', 'Shonnen Jump', '1647315058_b6d967caaf8867192fe9.jpg', NULL, NULL),
+(5, 'Black Clover', 'black-clover', 'Yuki Tabata', 'shonnen', '1647346372_4f7a9420ab6658a09ad2.jpg', NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `komik`
+--
+ALTER TABLE `komik`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `komik`
+--
+ALTER TABLE `komik`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
